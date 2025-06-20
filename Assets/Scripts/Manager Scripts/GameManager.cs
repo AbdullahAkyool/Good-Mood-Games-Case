6590e1ca7229346b4d0404158425fdbe5143f3d4
Abdullah Akyol <abdullah35akyol@gmail.com>
2025-06-21 01:12:17 +0300

    FEAT: GameManager

---
+++ Assets/Scripts/Manager Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        SetCursorLock(true);
+    }
+
+    public void SetCursorLock(bool lockCursor)
+    {
+        Cursor.lockState = lockCursor ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !lockCursor;
+    }
+}

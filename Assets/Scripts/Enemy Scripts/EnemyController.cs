cc92ae87c6c9131992658bd4636ef1ee001af308
Abdullah Akyol <abdullah35akyol@gmail.com>
2025-06-21 01:05:18 +0300

    REFACTOR: EnemyHealthController + EnemyController

--- Assets/Scripts/Enemy Scripts/EnemyController.cs
+++ Assets/Scripts/Enemy Scripts/EnemyController.cs
@@ -3,22 +3,46 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     private EnemyHealthController enemyHealthController;
+    public EnemyHealthController EnemyHealthController => enemyHealthController;
 
     private Animator animator;
     public Animator Animator => animator;
 
+    private Collider coll;
+    public Collider Coll => coll;
+
     public StateMachine stateMachine { get; private set; }
+
+    [SerializeField] private Renderer meshRenderer;
+    public Renderer MeshRenderer => meshRenderer;
+
+    public Rigidbody[] RagdollRigidbodies { get; private set; }
 
     private void Awake()
     {
         enemyHealthController = GetComponent<EnemyHealthController>();
+        coll = GetComponent<Collider>();
         animator = GetComponent<Animator>();
         stateMachine = new StateMachine();
+
+        RagdollRigidbodies = GetComponentsInChildren<Rigidbody>();
+        foreach (var rb in RagdollRigidbodies)
+            rb.isKinematic = true;
     }
 
     private void Start()
     {
         stateMachine.ChangeState(new EnemyIdleState(this));
+    }
+
+    public void EnableRagdoll()
+    {
+        foreach (var rb in RagdollRigidbodies)
+        {
+            rb.isKinematic = false;
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
     }
 
 }
--- Assets/Scripts/Enemy Scripts/EnemyHealthController.cs
+++ Assets/Scripts/Enemy Scripts/EnemyHealthController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealthController : MonoBehaviour
 {
@@ -7,6 +8,9 @@ public class EnemyHealthController : MonoBehaviour
     public int MaxHealth = 100;
     public int CurrentHealth = 100;
 
+    [SerializeField] private EnemyHealthBarController healthBarController;
+    public EnemyHealthBarController HealthBarController => healthBarController;
+
     void Awake()
     {
         enemyController = GetComponent<EnemyController>();
@@ -14,18 +18,39 @@ public class EnemyHealthController : MonoBehaviour
 
     private void Start()
     {
-        CurrentHealth = MaxHealth;
+        ResetHealthSystem();
     }
 
     public void TakeDamage(int damage)
     {
         CurrentHealth -= damage;
 
+        healthBarController.UpdateHealthBar(CurrentHealth / (float)MaxHealth);
+
         if (CurrentHealth <= 0)
         {
             CurrentHealth = 0;
 
             enemyController.stateMachine.ChangeState(new EnemyDieState(enemyController));
+        }
+
+        if (!(enemyController.stateMachine.CurrentState is EnemyGetHitState))
+        {
+            enemyController.stateMachine.ChangeState(new EnemyGetHitState(enemyController));
+        }
+    }
+    public void ResetHealthSystem()
+    {
+        healthBarController.fillBar.gameObject.SetActive(true);
+        CurrentHealth = MaxHealth;
+        healthBarController.UpdateHealthBar(1f);
+    }
+
+    public void OnEnemyGetHitEnd() // animation event
+    {
+        if (enemyController.stateMachine.CurrentState is EnemyGetHitState)
+        {
+            enemyController.stateMachine.ChangeState(new EnemyIdleState(enemyController));
         }
     }
 }

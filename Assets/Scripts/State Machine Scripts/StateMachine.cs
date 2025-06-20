c5d1bdc9ba3942192bc25d470106fe1e0bca1f53
Abdullah Akyol <abdullah35akyol@gmail.com>
2025-06-20 05:44:06 +0300

    FIX: File Organization

--- Assets/Scripts/State Machine Scripts/StateMachine.cs
+++ Assets/Scripts/State Machine Scripts/StateMachine.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class StateMachine
 {
     private IState currentState;
+    public IState CurrentState => currentState;
 
     public void ChangeState(IState newState)
     {
